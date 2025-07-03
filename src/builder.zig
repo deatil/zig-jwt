1a88572330d7ac3ba90f5b2c0fb28b9e43ca589c
deatil <2217957370@qq.com>
2025-07-03 13:43:19 +0800

    fixed

--- src/builder.zig
+++ src/builder.zig
@@ -9,6 +9,24 @@ const Values = std.ArrayList(u8);
 const eddsa = @import("eddsa.zig");
 const Token = @import("token.zig").Token;
 
+// Defines the list of headers that are registered in the IANA "JSON Web Token Headers" registry
+pub const RegisteredHeaders = struct {
+    pub const Type = "typ";
+    pub const Algorithm = "alg";
+    pub const Encryption = "enc";
+};
+
+// Defines the list of claims that are registered in the IANA "JSON Web Token Claims" registry
+pub const RegisteredClaims = struct {
+    pub const Audience = "aud";
+    pub const ExpirationTime = "exp";
+    pub const ID = "jti";
+    pub const IssuedAt = "iat";
+    pub const Issuer = "iss";
+    pub const NotBefore = "nbf";
+    pub const Subject = "sub";
+};
+
 pub fn Builder(comptime Signer: type, comptime SecretKeyType: type) type {
     return struct {
         signer: Signer,
@@ -32,12 +50,12 @@ pub fn Builder(comptime Signer: type, comptime SecretKeyType: type) type {
             self.claims.deinit();
         }
 
-        pub fn headersData(self: *Self) Data {
-            return Data.init(&self.headers);
+        pub fn headersData(self: *Self) HeadersData {
+            return HeadersData.init(&self.headers);
         }
 
-        pub fn claimsData(self: *Self) Data {
-            return Data.init(&self.claims);
+        pub fn claimsData(self: *Self) ClaimsData {
+            return ClaimsData.init(&self.claims);
         }
 
         pub fn getHeaders(self: *Self) ![]const u8 {
@@ -59,8 +77,8 @@ pub fn Builder(comptime Signer: type, comptime SecretKeyType: type) type {
                 defer h.deinit();
 
                 try h.begin();
-                try h.setData("typ", "JWT");
-                try h.setData("alg", self.signer.alg());
+                try h.typeBy("JWT");
+                try h.algoBy(self.signer.alg());
                 try h.end();
 
                 header = try self.getHeaders();
@@ -88,7 +106,7 @@ pub fn Builder(comptime Signer: type, comptime SecretKeyType: type) type {
     };
 }
 
-pub const Data = struct {
+pub const HeadersData = struct {
     stream: json.WriteStream(Values.Writer, .{ .checked_to_fixed_depth = 256 }),
 
     const Self = @This();
@@ -111,46 +129,83 @@ pub const Data = struct {
         try self.stream.endObject();
     }
 
-    pub fn permittedFor(self: *Self, audience: []const u8) !void {
-        try self.setData("aud", audience);
+    pub fn setData(self: *Self, name: []const u8, value: anytype) !void {
+        try self.stream.objectField(name);
+        try self.stream.write(value);
     }
 
-    pub fn expiresAt(self: *Self, expiration: i64) !void {
-        try self.setData("exp", expiration);
+    pub fn typeBy(self: *Self, typ: []const u8) !void {
+        try self.setData(RegisteredHeaders.Type, typ);
     }
 
-    pub fn identifiedBy(self: *Self, id: []const u8) !void {
-        try self.setData("jti", id);
+    pub fn algoBy(self: *Self, alg: []const u8) !void {
+        try self.setData(RegisteredHeaders.Algorithm, alg);
+    }
+};
+
+pub const ClaimsData = struct {
+    stream: json.WriteStream(Values.Writer, .{ .checked_to_fixed_depth = 256 }),
+
+    const Self = @This();
+
+    pub fn init(value: *Values) Self {
+        return .{
+            .stream = json.writeStream(value.*.writer(), .{ .emit_null_optional_fields = false }),
+        };
     }
 
-    pub fn issuedAt(self: *Self, issued_at: i64) !void {
-        try self.setData("iat", issued_at);
+    pub fn deinit(self: *Self) void {
+        self.stream.deinit();
     }
 
-    pub fn issuedBy(self: *Self, issuer: []const u8) !void {
-        try self.setData("iss", issuer);
+    pub fn begin(self: *Self) !void {
+        try self.stream.beginObject();
     }
 
-    pub fn canOnlyBeUsedAfter(self: *Self, not_before: i64) !void {
-        try self.setData("nbf", not_before);
-    }
-
-    pub fn relatedTo(self: *Self, subject: []const u8) !void {
-        try self.setData("sub", subject);
+    pub fn end(self: *Self) !void {
+        try self.stream.endObject();
     }
 
     pub fn setData(self: *Self, name: []const u8, value: anytype) !void {
         try self.stream.objectField(name);
         try self.stream.write(value);
     }
+
+    pub fn permittedFor(self: *Self, audience: []const u8) !void {
+        try self.setData(RegisteredClaims.Audience, audience);
+    }
+
+    pub fn expiresAt(self: *Self, expiration: i64) !void {
+        try self.setData(RegisteredClaims.ExpirationTime, expiration);
+    }
+
+    pub fn identifiedBy(self: *Self, id: []const u8) !void {
+        try self.setData(RegisteredClaims.ID, id);
+    }
+
+    pub fn issuedAt(self: *Self, issued_at: i64) !void {
+        try self.setData(RegisteredClaims.IssuedAt, issued_at);
+    }
+
+    pub fn issuedBy(self: *Self, issuer: []const u8) !void {
+        try self.setData(RegisteredClaims.Issuer, issuer);
+    }
+
+    pub fn canOnlyBeUsedAfter(self: *Self, not_before: i64) !void {
+        try self.setData(RegisteredClaims.NotBefore, not_before);
+    }
+
+    pub fn relatedTo(self: *Self, subject: []const u8) !void {
+        try self.setData(RegisteredClaims.Subject, subject);
+    }
 };
 
-test "Data" {
+test "ClaimsData" {
     const alloc = testing.allocator;
 
     var value = Values.init(alloc);
 
-    var b = Data.init(&value);
+    var b = ClaimsData.init(&value);
     defer b.deinit();
 
     try b.begin();
@@ -175,17 +230,41 @@ test "Data" {
     try testing.expectEqualStrings(check, claims);
 }
 
-test "Data 2" {
+test "HeadersData" {
     const alloc = testing.allocator;
 
     var value = Values.init(alloc);
 
-    var b = Data.init(&value);
+    var b = HeadersData.init(&value);
     defer b.deinit();
 
     try b.begin();
     try b.setData("typ", "JWT");
     try b.setData("alg", "ES256");
+    try b.end();
+
+    const check =
+        \\{"typ":"JWT","alg":"ES256"}
+    ;
+
+    const claims = try value.toOwnedSlice();
+
+    defer alloc.free(claims);
+
+    try testing.expectEqualStrings(check, claims);
+}
+
+test "HeadersData 2" {
+    const alloc = testing.allocator;
+
+    var value = Values.init(alloc);
+
+    var b = HeadersData.init(&value);
+    defer b.deinit();
+
+    try b.begin();
+    try b.typeBy("JWT");
+    try b.algoBy("ES256");
     try b.end();
 
     const check =
@@ -350,4 +429,18 @@ test "Builder 3" {
         \\{"typ":"JWT","alg":"EdDSA"}
     ;
     try testing.expectEqualStrings(check3, t.header);
+}
+
+test "Registered" {
+    try testing.expectEqualStrings("typ", RegisteredHeaders.Type);
+    try testing.expectEqualStrings("alg", RegisteredHeaders.Algorithm);
+    try testing.expectEqualStrings("enc", RegisteredHeaders.Encryption);
+
+    try testing.expectEqualStrings("aud", RegisteredClaims.Audience);
+    try testing.expectEqualStrings("exp", RegisteredClaims.ExpirationTime);
+    try testing.expectEqualStrings("jti", RegisteredClaims.ID);
+    try testing.expectEqualStrings("iat", RegisteredClaims.IssuedAt);
+    try testing.expectEqualStrings("iss", RegisteredClaims.Issuer);
+    try testing.expectEqualStrings("nbf", RegisteredClaims.NotBefore);
+    try testing.expectEqualStrings("sub", RegisteredClaims.Subject);
 }
